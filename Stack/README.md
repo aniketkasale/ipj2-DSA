fa00a0fb5b8328838896999404ec7d8d4e9bfc89
aniketkasale <aniketkasale02@gmail.com>
2024-08-08 07:38:37 +0530

    Stack

---
+++ Stack/README.md
@@ -0,0 +1,6 @@
+# STACK
+
+- Stack is a data structure that folows last in first out principle.
+- It behaves like a stack of boxes, where the last box added is the first and to be removed first.
+
+Consider stack as a simple array.
---
+++ Stack/index.js
@@ -0,0 +1,61 @@
+class Stack {
+  // initialize emplty stack at the begining
+  constructor() {
+    this.stack = [];
+  }
+
+  // Pushing data to the stack
+  push(data) {
+    this.stack.push(data);
+  }
+
+  // Popping data from the stack
+  pop() {
+    this.stack.pop();
+  }
+
+  // Peek - return last element of stack
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  // isEmpty
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  // Size - return size
+  size() {
+    return this.stack.length;
+  }
+
+  // Clear the stack
+  clear() {
+    this.stack = [];
+  }
+
+  // Contains - does this element contains the stack
+  contains(data) {
+    return this.stack.includes(data);
+  }
+
+  // reverse the stack
+  reverse() {
+    this.stack.reverse();
+  }
+
+  // print stack
+  printStack() {
+    let str = this.stack.map((element) => element + "\n");
+    return str.join("");
+  }
+}
+
+// Usage
+
+let myStack = new Stack();
+console.log("initial stack", myStack);
+myStack.push(1);
+myStack.push(2);
+console.log("peek", myStack.peek());
+console.log("print", myStack.printStack());
